44f4bc748932604db093300c3d8cf1cb120cc40f
jayanth-gopalan-bosch <138367478+jayanth-gopalan-bosch@users.noreply.github.com>
2023-07-03 23:28:27 +0530

    Update TypewiseAlertTest.java
    Added unit test case

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,12 +1,30 @@
 package TypewiseAlert;
 
 import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.internal.WhiteboxImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import TypewiseAlert.handler.alert.AlertHandlerFactory;
+import TypewiseAlert.handler.alert.AlertToControllerHandler;
+import TypewiseAlert.handler.alert.AlertToEmailHandler;
+import TypewiseAlert.handler.alert.IAlertHandler;
+import TypewiseAlert.types.AlertTarget;
 import TypewiseAlert.types.BreachType;
 import TypewiseAlert.types.CoolingType;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({AlertToControllerHandler.class, AlertToEmailHandler.class})
 public class TypewiseAlertTest {
 
     @Test
@@ -21,6 +39,30 @@ public class TypewiseAlertTest {
         assertSame(new TypewiseAlert().classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, -12.0f), BreachType.TOO_LOW);
         assertSame(new TypewiseAlert().classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 30.0f), BreachType.NORMAL);
         assertSame(new TypewiseAlert().classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 50.0f), BreachType.TOO_HIGH);
+    }
+
+    @Test
+    public void testCheckAndAlert() {
+        // Setup
+        PowerMockito.mockStatic(AlertHandlerFactory.class);
+        Map<AlertTarget, IAlertHandler> alertHandlerMap = new HashMap<>();
+        AlertToControllerHandler mockAlertToControllerHandler = mock(AlertToControllerHandler.class);
+        AlertToEmailHandler mockAlertToEmailHandler = mock(AlertToEmailHandler.class);
+
+        alertHandlerMap.put(AlertTarget.TO_CONTROLLER, mockAlertToControllerHandler);
+        alertHandlerMap.put(AlertTarget.TO_EMAIL, mockAlertToEmailHandler);
+
+        WhiteboxImpl.setInternalState(AlertHandlerFactory.class, "sAlertHandlerMap", alertHandlerMap);
+
+        // Call target APIs
+        new TypewiseAlert().checkAndAlert(AlertTarget.TO_CONTROLLER,
+                new BatteryCharacter(CoolingType.MED_ACTIVE_COOLING, ""), 35);
+        new TypewiseAlert().checkAndAlert(AlertTarget.TO_EMAIL,
+                new BatteryCharacter(CoolingType.MED_ACTIVE_COOLING, ""), 35);
+
+        // Verify
+        verify(mockAlertToControllerHandler, Mockito.atLeastOnce()).onHandle(BreachType.NORMAL);
+        verify(mockAlertToEmailHandler, Mockito.atLeastOnce()).onHandle(BreachType.NORMAL);
     }
 
 }
