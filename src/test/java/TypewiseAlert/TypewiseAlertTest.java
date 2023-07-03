a0fe6ad995a189e3df2af854fb61496679a06b31
jayanth-gopalan-bosch <138367478+jayanth-gopalan-bosch@users.noreply.github.com>
2023-07-04 00:11:02 +0530

    Update TypewiseAlertTest.java
    Updated test cases

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -13,15 +13,10 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.reflect.internal.WhiteboxImpl;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import TypewiseAlert.handler.alert.AlertHandlerFactory;
 import TypewiseAlert.handler.alert.AlertToControllerHandler;
 import TypewiseAlert.handler.alert.AlertToEmailHandler;
-import TypewiseAlert.handler.alert.IAlertHandler;
 import TypewiseAlert.types.AlertTarget;
 import TypewiseAlert.types.BreachType;
 import TypewiseAlert.types.CoolingType;
@@ -43,24 +38,40 @@ public class TypewiseAlertTest {
     }
 
     @Test
-    public void infersBreachAsPerLimits() {
+    public void infersBreachAsPerLimitsTooLow() {
         assertSame(mTypewiseAlert.inferBreach(12, 20, 30), BreachType.TOO_LOW);
+    }
+
+    @Test
+    public void infersBreachAsPerLimitsNormal() {
         assertSame(mTypewiseAlert.inferBreach(25, 20, 30), BreachType.NORMAL);
+    }
+
+    @Test
+    public void infersBreachAsPerLimitsTooHigh() {
         assertSame(mTypewiseAlert.inferBreach(32, 20, 30), BreachType.TOO_HIGH);
     }
 
     @Test
-    public void testClassifyTemperatureBreach() {
+    public void testClassifyTemperatureBreachPassiveCooling() {
         assertSame(mTypewiseAlert.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, -12.0f),
                 BreachType.TOO_LOW);
+    }
+
+    @Test
+    public void testClassifyTemperatureBreachMedActiveCooling() {
         assertSame(mTypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 30.0f),
                 BreachType.NORMAL);
+    }
+
+    @Test
+    public void testClassifyTemperatureBreachHiActiveCooling() {
         assertSame(mTypewiseAlert.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 50.0f),
                 BreachType.TOO_HIGH);
     }
 
     @Test
-    public void testCheckAndAlert1() {
+    public void testCheckAndAlertController() {
         // Setup
         PowerMockito.mockStatic(AlertHandlerFactory.class);
         AlertToControllerHandler mockAlertToControllerHandler = mock(AlertToControllerHandler.class);
@@ -75,7 +86,7 @@ public class TypewiseAlertTest {
     }
 
     @Test
-    public void testCheckAndAlert2() {
+    public void testCheckAndAlertEmail() {
         // Setup
         PowerMockito.mockStatic(AlertHandlerFactory.class);
         AlertToEmailHandler mockAlertToEmailHandler = mock(AlertToEmailHandler.class);
